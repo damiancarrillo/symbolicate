5c64de7c35495621ff0644e047d4abb56383b52a
Damian Carrillo <dcarrillo@atlassian.com>
2019-02-25 17:08:18 -0600

    Producing a valid highlighted crash log

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@
 extern crate clap;
 extern crate regex;
 
+use ansi_term::Style;
 use clap::{App, Arg};
 use regex::Regex;
 use std::fs::File;
-use std::io::{self, prelude::*, BufReader, Result, SeekFrom, Write};
+use std::io::{prelude::*, BufReader, Result, SeekFrom};
 use std::process::Command;
-use ansi_term::Style;
 
 // https://developer.apple.com/library/archive/technotes/tn2151/_index.html#//apple_ref/doc/uid/DTS40008184-CH1-SYMBOLICATE_WITH_ATOS
 
@@ -16,20 +16,39 @@ fn main() -> Result<()> {
         .version(crate_version!())
         .author(crate_authors!("\n"))
         .about(crate_description!())
-        .arg(Arg::with_name("crash_file_path").required(true).index(1))
+        .arg(
+            Arg::with_name("crash_log")
+                .required(true)
+                .short("c")
+                .long("crash_log")
+                .takes_value(true)
+                .help("The path to a crash log"),
+        )
+        .arg(
+            Arg::with_name("app")
+                .required(true)
+                .short("a")
+                .long("app")
+                .takes_value(true)
+<<<<<<< HEAD
+                .help("The path to an application binary (eg. Some.app/Some)"),
+=======
+                .help("The path to an application binary (eg. Trello.app/Trello)"),
+>>>>>>> 5c64fab... Producing a valid highlighted crash log
+        )
         .get_matches();
 
-    let crash_file_path = matches.value_of("crash_file_path").unwrap();
-    let mut crash_file = File::open(crash_file_path)?;
+    let crash_log_path = matches.value_of("crash_log").unwrap();
+    let mut crash_file = File::open(crash_log_path)?;
+
+    let app = matches.value_of("app").unwrap();
 
     let arch = determine_architecture(&crash_file)
-        .expect(format!("Could not determine architecture from {}", crash_file_path).as_str());
+        .expect(format!("Could not determine architecture from {}", crash_log_path).as_str());
 
     println!("{}", arch);
 
     crash_file.seek(SeekFrom::Start(0))?;
-
-    // xcrun atos -o Trello.app/Trello -arch arm64 -l 0x102194000 0x102b60ffc
 
     // 2    XYZLib    0x34648e88    0x83000 + 8740
     let regex = Regex::new(r"^(.*)(0x[a-fA-F0-9]+) (0x[a-fA-F0-9]+) .*$")
@@ -41,28 +60,33 @@ fn main() -> Result<()> {
         .take_while(|l| !l.starts_with("Binary Images:"))
     {
         if let Some(captures) = regex.captures(&line) {
-            if let (Some(a), Some(b), Some(c)) = (
-                &captures.get(1),
-                &captures.get(2),
-                &captures.get(3),
-            ) {
+            if let (Some(prefix), Some(method_address), Some(main_address)) =
+                (&captures.get(1), &captures.get(2), &captures.get(3))
+            {
+                // xcrun atos -o Trello.app/Trello -arch arm64 -l 0x102194000 0x102b60ffc
                 let result = Command::new("xcrun")
                     .args(&[
                         "atos",
                         "-o",
-                        "Trello.app/Trello",
+                        app,
                         "-arch",
                         &arch,
                         "-l",
-                        c.as_str(),
-                        b.as_str(),
+                        main_address.as_str(),
+                        method_address.as_str(),
                     ])
                     .output();
                 if let Ok(output) = result {
-                    // println!("yo yo yo");
-                    print!("{}{} ", Style::new().bold().paint(a.as_str()), Style::new().bold().paint(b.as_str()));
-                    io::stdout().write_all(&output.stdout).unwrap();
-                    continue;
+                    if let Ok(symbolicated_line) = String::from_utf8(output.stdout) {
+                        let style = Style::new().bold();
+                        println!(
+                            "{}{} {}",
+                            style.paint(prefix.as_str()),
+                            style.paint(method_address.as_str()),
+                            style.paint(symbolicated_line.trim())
+                        );
+                        continue;
+                    }
                 }
             }
         }
